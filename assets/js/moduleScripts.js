4574c39cb684fef7943d11cd06421ede53d75313
BeckhamEldridge <beckham.eldridge@gmail.com>
2024-09-24 21:01:21 -0400

    trying to fix error - try 5

---
+++ assets/js/moduleScripts.js
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function fetchCategories() {
+    const { data, error } = await supabase
+        .from('Categories')
+        .select('*');
+    if (error) console.error(error);
+    else console.log(data);
+}
+
+fetchCategories();
+
+   <script type="module" src="assets/js/scripts.js"></script>
