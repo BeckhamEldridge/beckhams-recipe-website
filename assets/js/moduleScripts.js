325724b66f09629c123122a26dee0b6636578ab0
BeckhamEldridge <beckham.eldridge@gmail.com>
2024-09-28 14:25:01 -0400

    Removed moduleScripts.js

--- assets/js/moduleScripts.js
+++
@@ -1,7 +0,0 @@
-// Import the Supabase client creation module
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase with environment variables
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
